b7f38ab2efd69290e01eb6c29887eefd055dc613
Doni <doniattokurovich@gmail.com>
2020-07-07 15:19:49 +0600

    yes

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ let output = document.querySelector("output");
 let key = document.createElement("div");
 key.classList.add("keys");
 calculator.appendChild(key);
-"C/*-+123456789=".split("").map((item) => {
+"C/*-123+4560789=".split("").map((item) => {
   key.insertAdjacentHTML(
     "beforeend",
     `<button value="${item}">${item}</button>`
@@ -12,19 +12,14 @@ calculator.appendChild(key);
 });
 key.addEventListener("click", function (evt) {
   let target = evt.target;
-  let num1 = 0;
-  let num2 = 0;
+
   if (target.tagName == "BUTTON") {
     output.textContent += target.textContent;
   }
   if (target.value == "C") {
     output.textContent = "";
   }
-  if (target.textContent == "+") {
-    // num1 = output.value;
 
-    console.log(num1);
-  }
   if (target.textContent == "=") {
     // let res = output.textContent.split("");
     // let res2 = res.pop();
